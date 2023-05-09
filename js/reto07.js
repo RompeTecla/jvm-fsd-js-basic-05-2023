70a55b6249e344431d068580e0d522a8d7e1a4e4
RompeTecla <javalinogames@gmail.com>
2023-05-09 20:14:43 +0200

    ordered html and js files

--- js/reto07.js
+++ js/reto07.js
@@ -1,8 +1,8 @@
 //script que permita escribir los (numero que elija el usuario) primeros numeros pares.
-let nPares = prompt("cuantos numeros de pares quieres?")
+let nVeces = prompt("cuantos numeros de pares quieres?")
 let nPar = 0
 
-for (let index = 0; index < nPares; index ++){
+for (let index = 0; index < nVeces; index ++){
     console.log(nPar)
     nPar = nPar + 2;
 }
